da3e2de4383dbe9e6e94f727cb64a22c1eae31fd
Cláudio Ribeiro <claudio.santos.ribeiro@gmail.com>
2017-05-02 11:09:56 +0100

    Added support for spotify

--- startup.sh
+++ startup.sh
@@ -39,6 +39,12 @@ apt-get install weechat -y
 apt-get install lynx -y
 apt-get install mutt -y
 
+################ For Spotify
+# apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+# deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+# apt-get update
+# apt-get install spotify-client
+
 ############### Update all packages
 apt-get update
 
