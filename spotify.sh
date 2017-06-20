0bef2f6eccb7246ca9969b0bcfad85bccfeb363a
Claudio Ribeiro <cribeiro@ventureoak.com>
2017-06-20 10:14:58 +0100

    Corrected spotify script

--- spotify.sh
+++ spotify.sh
@@ -4,8 +4,14 @@
 # This script will install spotify on your linux system
 ##############################
 
-################ For Spotify
+# 1. Add the Spotify repository signing key to be able to verify downloaded packages
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
-deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# 2. Add the Spotify repository
+echo deb http://repository.spotify.com stable non-free | sudo tee /etc/apt/sources.list.d/spotify.list
+
+# 3. Update list of available packages
 apt-get update
+
+# 4. Install Spotify
 apt-get install spotify-client
