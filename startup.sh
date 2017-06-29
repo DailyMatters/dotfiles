76ed8163fb4eb29f6793535ea0d918e2b145c54c
Cláudio Ribeiro <claudio.santos.ribeiro@gmail.com>
2017-06-29 10:16:16 +0100

    Update startup script with phpunit

--- startup.sh
+++ startup.sh
@@ -26,6 +26,9 @@ apt-get update
 
 apt-get install php7.0 -y
 
+# Some phpunit for good measure
+apt-get install phpunit -y
+
 # For composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
