d7e291ea021381184c583a0e9af82abc083f4d25
Claudio Ribeiro <claudio.santos.ribeiro@gmail.com>
2018-03-06 19:31:53 +0000

    Major changes on packages

--- startup.sh
+++ startup.sh
@@ -21,19 +21,20 @@ apt-get install gitk -y
 
 # For php7
 apt-get install python-software-properties software-properties-common
-LC_ALL=C.UTF-8 add-apt-repository ppa:ondrej/php
-apt-get update
 
-apt-get install php7.0 -y
+sudo add-apt-repository ppa:ondrej/php
+sudo apt-get update
+sudo apt-get install php7.1
 
 # Some phpunit for good measure
 apt-get install phpunit -y
 
 # For composer
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php -r "if (hash_file('SHA384', 'composer-setup.php') === '544e09ee996cdf60ece3804abc52599c22b1f40f4323403c44d44fdfdd586475ca9813a858088ffbc1f233e9b180f061') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
 php composer-setup.php
 php -r "unlink('composer-setup.php');"
+
 
 # Move it to the path to make it available through the 'Composer' command
 mv composer.phar /usr/local/bin/composer
